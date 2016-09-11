a6a2fecfa3ff15ea54c96bdad81b1ebc96a23620
MillerOs <836985396@qq.com>
2016-09-11 15:50:48 +0800

    将分类独立页面合并到主页上。

--- js/Ani.js
+++ js/Ani.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	var screenHeight = $(window).height();
 	var containerHeight = $(".container").height();
 	$(".showPage").eq(0).addClass("showPageOn");
-//	$(".SlectList li").eq(0).addClass("SlectListLiHover");
+	$(".SlectList li").eq(0).addClass("SlectListLiHover");
 	$(".SlectList li").click(function(){
 		$(this).addClass("SlectListLiHover").siblings().removeClass("SlectListLiHover");
 	});
@@ -14,47 +14,53 @@ $(document).ready(function(){
 		$(this).addClass("footButtonHover").siblings().removeClass("footButtonHover");
 	});
 	
-	$(".ordinal").click(function(){
-		var index = $(this).index() - 1;
+//	$(".ordinal").click(function(){
+//		var index = $(this).index() - 1;
+//		$(".showPage").eq(index).addClass("showPageOn").siblings().removeClass("showPageOn");
+//	});
+
+
+	$(".SlectList li").click(function(){
+		var index = $(this).index();
 		$(".showPage").eq(index).addClass("showPageOn").siblings().removeClass("showPageOn");
 	});
 	
 	$(".ordinal").eq(0).addClass("footButtonHover");
 
 
-	$(".next").click(function(){
-		var pageIndex = $(".showPage:visible").index();
-		if( pageIndex + 1 == $(".ordinal").length){
-			$(".ordinal").eq( pageIndex ).addClass("footButtonHover");
-			$(".ordinal").eq( pageIndex ).siblings().removeClass("footButtonHover");
-			alert("别点了，到底了喔~");
-		}else{
-			$(".showPage").eq( pageIndex + 1 ).addClass("showPageOn");
-			$(".showPage").eq( pageIndex + 1 ).siblings().removeClass("showPageOn");
-			$(".ordinal").eq( pageIndex + 1 ).addClass("footButtonHover");
-			$(".ordinal").eq( pageIndex + 1 ).siblings().removeClass("footButtonHover");
-		}
-	});
-	$(".forward").click(function(){
-		var pageIndexes = $(".showPage:visible").index();
-		if( pageIndexes == 0 ){
-			$(".ordinal").eq( pageIndexes ).addClass("footButtonHover");
-			$(".ordinal").eq( pageIndexes ).siblings().removeClass("footButtonHover");
-			alert("别点了，到顶了喔~");
-		}else{
-			$(".showPage").eq( pageIndexes - 1 ).addClass("showPageOn");
-			$(".showPage").eq( pageIndexes - 1 ).siblings().removeClass("showPageOn");
-			$(".ordinal").eq( pageIndexes - 1 ).addClass("footButtonHover");
-			$(".ordinal").eq( pageIndexes - 1 ).siblings().removeClass("footButtonHover");
-		}
-	});
-	$(".GObutton").click(function(){
-		var GoIndex = $(".goToPage input").val();
-		$(".showPage").eq( GoIndex - 1 ).addClass("showPageOn");
-		$(".showPage").eq( GoIndex - 1 ).siblings().removeClass("showPageOn");
-		$(".ordinal").eq( GoIndex - 1 ).addClass("footButtonHover");
-		if(GoIndex == null || GoIndex > $(".ordinal").length ){
-			alert("你要确定你的要求我们可以办到喔:(");
-		}
-	})
+//	$(".next").click(function(){
+//		var pageIndex = $(".showPage:visible").index();
+//		if( pageIndex + 1 == $(".ordinal").length){
+//			$(".ordinal").eq( pageIndex ).addClass("footButtonHover");
+//			$(".ordinal").eq( pageIndex ).siblings().removeClass("footButtonHover");
+//			alert("别点了，到底了喔~");
+//		}else{
+//			$(".showPage").eq( pageIndex + 1 ).addClass("showPageOn");
+//			$(".showPage").eq( pageIndex + 1 ).siblings().removeClass("showPageOn");
+//			$(".ordinal").eq( pageIndex + 1 ).addClass("footButtonHover");
+//			$(".ordinal").eq( pageIndex + 1 ).siblings().removeClass("footButtonHover");
+//		}
+//	});
+//	$(".forward").click(function(){
+//		var pageIndexes = $(".showPage:visible").index();
+//		if( pageIndexes == 0 ){
+//			$(".ordinal").eq( pageIndexes ).addClass("footButtonHover");
+//			$(".ordinal").eq( pageIndexes ).siblings().removeClass("footButtonHover");
+//			alert("别点了，到顶了喔~");
+//		}else{
+//			$(".showPage").eq( pageIndexes - 1 ).addClass("showPageOn");
+//			$(".showPage").eq( pageIndexes - 1 ).siblings().removeClass("showPageOn");
+//			$(".ordinal").eq( pageIndexes - 1 ).addClass("footButtonHover");
+//			$(".ordinal").eq( pageIndexes - 1 ).siblings().removeClass("footButtonHover");
+//		}
+//	});
+//	$(".GObutton").click(function(){
+//		var GoIndex = $(".goToPage input").val();
+//		$(".showPage").eq( GoIndex - 1 ).addClass("showPageOn");
+//		$(".showPage").eq( GoIndex - 1 ).siblings().removeClass("showPageOn");
+//		$(".ordinal").eq( GoIndex - 1 ).addClass("footButtonHover");
+//		if(GoIndex == null || GoIndex > $(".ordinal").length ){
+//			alert("你要确定你的要求我们可以办到喔:(");
+//		}
+//	})
 })
